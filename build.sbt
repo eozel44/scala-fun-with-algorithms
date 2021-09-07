5ed1d5a69f257c790a6ec69d1c550e063d20d554
eren <eozel44@gmail.com>
2021-09-07 12:18:01 +0300

    spriral traverse matrix problem

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "scalasamples"
+
+version := "0.1"
+
+scalaVersion := "2.13.6"
---
+++ src/main/scala/matrix/spiralMatrix.scala
@@ -0,0 +1,35 @@
+package matrix
+
+object spiralMatrix {
+
+  /***
+   * Problem: Matrix in spiral format.
+
+Input:  1    2   3   4
+        5    6   7   8
+        9   10  11  12
+        13  14  15  16
+Output: 1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+   */
+
+  def main(args: Array[String]): Unit = {
+
+    val matrix = List(List(1, 2, 3, 4), List(5, 6, 7, 8), List(9,10,11,12),List(13,14,15,16))
+    matrix.map(println)
+
+    def spiralTraverseMatrix(l:List[List[Int]],acc:List[List[Int]]):List[List[Int]] = {
+      l match {
+        case  Nil => acc
+        case head::tail => {
+          val remaining = tail.transpose.reverse
+          spiralTraverseMatrix(remaining,head::acc)
+        }
+      }
+    }
+ val result = spiralTraverseMatrix(matrix,List.empty).reverse.flatten
+
+    result.foreach(k=>print(k+" "))
+
+
+  }
+}
